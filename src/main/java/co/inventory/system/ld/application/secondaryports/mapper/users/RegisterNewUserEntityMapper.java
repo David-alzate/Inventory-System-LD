49ee63c237bf871862401ea4e0a373c16afa59a2
David-alzate <alzatedavid0126@gmail.com>
2024-12-08 22:49:35 -0500

    Add Mappers for User and userType for register new user

---
+++ src/main/java/co/inventory/system/ld/application/secondaryports/mapper/users/RegisterNewUserEntityMapper.java
@@ -0,0 +1,23 @@
+package co.inventory.system.ld.application.secondaryports.mapper.users;
+
+
+import co.inventory.system.ld.application.secondaryports.entity.users.UserEntity;
+import co.inventory.system.ld.domain.users.UserDomain;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper
+public interface RegisterNewUserEntityMapper {
+
+    RegisterNewUserEntityMapper INSTANCE = Mappers.getMapper(RegisterNewUserEntityMapper.class);
+
+    UserEntity toEntity(UserDomain domain);
+
+    UserDomain toDomain(UserEntity entity);
+
+    List<UserEntity> toEntityCollection(List<UserDomain> domainCollection);
+
+    List<UserDomain> toDomainCollection(List<UserEntity> entityCollection);
+}
