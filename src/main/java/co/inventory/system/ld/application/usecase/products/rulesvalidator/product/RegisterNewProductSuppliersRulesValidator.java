6e98cf93ab4b963d2d6ff70f73ec59c469b6bf3d
Lauraagudelo04 <Lauraagudelomarulanda1@gmail.com>
2024-12-25 19:28:05 -0500

    Add Rules Validator implementation for name related with products

---
+++ src/main/java/co/inventory/system/ld/application/usecase/products/rulesvalidator/product/RegisterNewProductSuppliersRulesValidator.java
@@ -0,0 +1,7 @@
+package co.inventory.system.ld.application.usecase.products.rulesvalidator.product;
+
+import co.inventory.system.ld.application.usecase.RulesValidator;
+import co.inventory.system.ld.domain.products.ProductDomain;
+
+public interface RegisterNewProductSuppliersRulesValidator extends RulesValidator<ProductDomain> {
+}
