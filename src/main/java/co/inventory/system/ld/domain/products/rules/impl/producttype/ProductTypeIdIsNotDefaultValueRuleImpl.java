a1c411840a50f20435a00240ab3c3ac47ebba844
David-alzate <alzatedavid0126@gmail.com>
2024-12-27 22:05:35 -0500

    Add rules implementation for product type in domain

--- src/main/java/co/inventory/system/ld/domain/products/rules/impl/producttype/ProductTypeIdIsNotDefaultValueRuleImpl.java
+++ src/main/java/co/inventory/system/ld/domain/products/rules/impl/producttype/ProductTypeIdIsNotDefaultValueRuleImpl.java
@@ -1,13 +1,18 @@
 package co.inventory.system.ld.domain.products.rules.impl.producttype;
 
-import co.inventory.system.ld.domain.products.rules.producttype.ProductTypeIdIsNotDefaultValueRule;
-
 import java.util.UUID;
+
+import co.inventory.system.ld.crosscutting.helpers.UUIDHelper;
+import co.inventory.system.ld.domain.products.exceptions.producttype.ProductTypeIdIsDefaultValueException;
+import co.inventory.system.ld.domain.products.rules.producttype.ProductTypeIdIsNotDefaultValueRule;
 
 public class ProductTypeIdIsNotDefaultValueRuleImpl implements ProductTypeIdIsNotDefaultValueRule {
 
-    @Override
-    public void validate(UUID data) {
+	@Override
+	public void validate(UUID data) {
+		if (UUIDHelper.isDefault(data)) {
+			throw ProductTypeIdIsDefaultValueException.create();
+		}
 
-    }
+	}
 }
--- src/main/java/co/inventory/system/ld/domain/products/rules/impl/producttype/ProductTypeIdIsNotNullRuleImpl.java
+++ src/main/java/co/inventory/system/ld/domain/products/rules/impl/producttype/ProductTypeIdIsNotNullRuleImpl.java
@@ -1,16 +1,16 @@
 package co.inventory.system.ld.domain.products.rules.impl.producttype;
 
-import co.inventory.system.ld.crosscutting.helpers.ObjectHelper;
-import co.inventory.system.ld.domain.customers.exceptions.CustomerIdIsNullException;
-import co.inventory.system.ld.domain.products.rules.producttype.ProductTypeIdIsNotNullRule;
-
 import java.util.UUID;
+
+import co.inventory.system.ld.crosscutting.helpers.ObjectHelper;
+import co.inventory.system.ld.domain.products.exceptions.producttype.ProductTypeIdIsNullException;
+import co.inventory.system.ld.domain.products.rules.producttype.ProductTypeIdIsNotNullRule;
 
 public class ProductTypeIdIsNotNullRuleImpl implements ProductTypeIdIsNotNullRule {
     @Override
     public void validate(UUID data) {
         if (ObjectHelper.isNull(data)){
-            throw CustomerIdIsNullException.create();
+            throw ProductTypeIdIsNullException.create();
         }
     }
 }
