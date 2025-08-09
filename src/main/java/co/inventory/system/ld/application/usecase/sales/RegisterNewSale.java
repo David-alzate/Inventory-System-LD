95a8e3460f069ae9b91f71835e91990035a950bc
Laura Agudelo <142184444+lauraagudelo04@users.noreply.github.com>
2025-08-08 22:57:55 -0500

    Add RegisterNewSale class and interface to start implementing the use case

---
+++ src/main/java/co/inventory/system/ld/application/usecase/sales/RegisterNewSale.java
@@ -0,0 +1,7 @@
+package co.inventory.system.ld.application.usecase.sales;
+
+import co.inventory.system.ld.application.usecase.UseCaseWithOutRetuen;
+import co.inventory.system.ld.domain.sales.SaleDomain;
+
+public interface RegisterNewSale extends UseCaseWithOutRetuen<SaleDomain> {
+}
---
+++ src/main/java/co/inventory/system/ld/application/usecase/sales/impl/RegisterNewSaleImpl.java
@@ -0,0 +1,13 @@
+package co.inventory.system.ld.application.usecase.sales.impl;
+
+import co.inventory.system.ld.application.usecase.sales.RegisterNewSale;
+import co.inventory.system.ld.domain.sales.SaleDomain;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegisterNewSaleImpl implements RegisterNewSale {
+    @Override
+    public void execute(SaleDomain domain) {
+
+    }
+}
