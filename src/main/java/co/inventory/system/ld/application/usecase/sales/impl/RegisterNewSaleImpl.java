951f78602322037765f5bc74da66cbea79dc0ac3
Laura Agudelo <142184444+lauraagudelo04@users.noreply.github.com>
2025-08-09 22:18:44 -0500

    Add method to save a sale

--- src/main/java/co/inventory/system/ld/application/usecase/sales/impl/RegisterNewSaleImpl.java
+++ src/main/java/co/inventory/system/ld/application/usecase/sales/impl/RegisterNewSaleImpl.java
@@ -1,13 +1,24 @@
 package co.inventory.system.ld.application.usecase.sales.impl;
 
+import co.inventory.system.ld.application.secondaryports.entity.sales.SaleEntity;
+import co.inventory.system.ld.application.secondaryports.mapper.sales.SaleEntityMapper;
+import co.inventory.system.ld.application.secondaryports.repository.sales.SaleRepository;
 import co.inventory.system.ld.application.usecase.sales.RegisterNewSale;
 import co.inventory.system.ld.domain.sales.SaleDomain;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RegisterNewSaleImpl implements RegisterNewSale {
+
+    private final SaleRepository saleRepository;
+
+    public RegisterNewSaleImpl(SaleRepository saleRepository) {
+        this.saleRepository = saleRepository;
+    }
+
     @Override
     public void execute(SaleDomain domain) {
-
+        SaleEntity saleEntity = SaleEntityMapper.INSTANCE.toEntity(domain);
+        saleRepository.save(saleEntity);
     }
 }
