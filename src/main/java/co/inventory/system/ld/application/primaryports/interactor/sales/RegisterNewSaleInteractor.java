636218bc2698e143a35440d809bc2e34ae19af91
Laura Agudelo <142184444+lauraagudelo04@users.noreply.github.com>
2025-08-10 22:21:46 -0500

    Add RegisterNewSaleInteractor interface

---
+++ src/main/java/co/inventory/system/ld/application/primaryports/interactor/sales/RegisterNewSaleInteractor.java
@@ -0,0 +1,7 @@
+package co.inventory.system.ld.application.primaryports.interactor.sales;
+
+import co.inventory.system.ld.application.primaryports.dto.sales.SaleDTO;
+import co.inventory.system.ld.application.primaryports.interactor.InteractorWithOutReturn;
+
+public interface RegisterNewSaleInteractor extends InteractorWithOutReturn<SaleDTO> {
+}
